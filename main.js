2f595391861614e3006872b3f3dd2a4a87511945
Mark Jones <mark.jones@salem.edu>
2022-12-14 10:52:44 -0500

    feat(jobs): load jobs from JSON

--- main.js
+++ main.js
@@ -2,12 +2,26 @@ import './style.css';
 import javascriptLogo from './javascript.svg';
 import { setupCounter } from './counter.js';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`;
+// https://app.clearcompany.com/api/v2/jobs/salemacademy/xml/
+// https://app.clearcompany.com/api/v2/jobs/salemacademy/json/
 
-setupCounter(document.querySelector('#counter'));
+function summary(description) {
+  return description.substr(0,400)
+}
+
+async function getData() {
+  const response = await fetch('https://app.clearcompany.com/api/v2/jobs/salemacademy/json/')
+  const rdata = await response.json()
+  console.table(rdata)
+  const jobs = document.querySelector('#jobs')
+  jobs.innerHTML = rdata.reduce((p,c,i) => {
+    return `${p} <h3><a href="${c.LegacyCareersUrl}">${c.PositionTitle}</a></h3>
+    <p>${summary(c.Description)}</p>
+    <div><a href="${c.ApplyUrl}">Apply for ${c.PositionTitle}</a></div>`
+  },'')
+
+}
+
+
+// setupCounter(document.querySelector('#counter'));
+getData()
